4b860bd1085787c191ea1e8db57be83246aeb31c
08102357255 <aitandayoj1@gmail.com>
2022-09-12 19:46:09 -0700

    C

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#iclude <stdio.h>
+/**
+ * main - lowercase 
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	charlower[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+		putcher(lower[i]);
+	puthcer('\n');
+	return (0);
+}
